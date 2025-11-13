3200b4fcf383bc7fd646297b8e14ac0e8871a86d
Manjeet Singh <manjeetikon@gmail.com>
2025-11-13 12:08:22 +0530

    Performing basic operation

---
+++ Day1.py
@@ -0,0 +1,11 @@
+a=10
+b=20
+if a > 20 :
+    print(f"This is less than 30 and value of a is {a}")
+elif a<10:
+    print(f"This is smaller than 10 and value of a is {a}")
+else:
+    print(f"God knows what is this and value of a is {a}")
+
+name = int(input("Enter your name for enrollment:"))
+print("Name of the candidate is ", name, type(name))
