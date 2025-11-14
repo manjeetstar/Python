b068e608d2b0d744466214f6ed3e632f9845f83a
Manjeet Singh <manjeetikon@gmail.com>
2025-11-14 20:44:30 +0530

    Added functional & lambda expression

--- Day2-3.py
+++ Day2-3.py
@@ -8,3 +8,8 @@ t1=()
 t2={}
 t1, t2= validate(10,20, "Manjeet", 7, 20, name="Manjeet", age=20, address="Canada")
 print(f"return value is {t1} and {t2}")
+
+nums = [1,2,3,4,5,6]
+evens = list(map(lambda x: x % 2 == 0, nums))
+evens1=list([1,2,3,4,5])
+print(evens1)
