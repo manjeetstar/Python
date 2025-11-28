389147e07265bdb6ae60c3de37ef8cc2ed1e88b3
Manjeet Singh <manjeetikon@gmail.com>
2025-11-28 17:30:37 +0530

    Added DS part

--- Set2/Day2.py
+++ Set2/Day2.py
@@ -1,11 +1,28 @@
-name="Manjeet Singh Phull aeiou"
-print(name[3:-8:-1])
+from collections import namedtuple
 
-vowel, count ="aeiou", 0
+names=["Manjeet",20, "Parul", False, "Awatar", "Manjeet"]
+
+details = [name for name in names if type(name) == str]
+print(details)
+
+a=names.count("Manjeet")
+print(a)
+
+x=[x for x in range(1,100) if x%2 ==0]
+print(x)
+
+ctr=[y for y in "abchjdlfooqroqroqowhr" if y in "aeiou"]
+print(f"No. of vowels present in string is {len(ctr)}")
+
+names.extend(["A",8,3,3,2])
+print(names)
+
+nTuple=("Manjeet",10,20,"Awatar", False,"Satyam")
+print(nTuple)
+
+Candidate=namedtuple("Candidate", "name address city zip")
+n=Candidate("Manjeet", "Canada","Vancouver", 20220)
+
+print(n.name, n.address, n.city, n.zip, end=" ")
 
 
-for c in name:
-    if c in vowel:
-        count +=1
-
-print(f"No of vowels is {count}")
