926273102d083f190b637ede55703d6aac650387
Manjeet Singh <manjeetikon@gmail.com>
2025-12-10 08:37:45 +0530

    Added Encapsulation

--- Set4/Day2-1.py
+++ Set4/Day2-1.py
@@ -2,9 +2,10 @@ class Employee:
     country="Canada"
     __dob="930202"
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, gender):
         self.name=name
         self.age=age
+        self.__gender=gender
 
     def display(self):
         print(f"Name and age of candidate is {self.name} and {self.age}")
@@ -17,12 +18,7 @@ class Employee:
     def calculate(a, b):
         print(f"Sum of two numbers is {a+b}")
 
-e1=Employee("Manjeet", 20)
-e2=Employee("Parul", 19)
+e1=Employee("Manjeet", 20,"Male")
 e1.display()
-e2.display()
 e1.displayOrigin()
-e2.displayOrigin()
-e1.calculate(5,10)
-print(e1._Employee__dob)
-print(Employee.__dict__)
+print(e1.__dict__)
