0bbf92125e80707509bd3cfc6c5710ee44c33a1f
Manjeet Singh <manjeetikon@gmail.com>
2025-12-16 13:19:24 +0530

    Added data classes concept

--- Set8/Day6-1.py
+++ Set8/Day6-1.py
@@ -1,5 +1,18 @@
-from typing import Any
-def calculate(a:int=20)->Any:
-    print(f"Default value of a is {a}")
+from dataclasses import dataclass, field
 
-print(calculate())
+@dataclass(repr=True)
+class employee:
+    name:str
+    age: int
+    isIndian: bool
+    country:list[str]=field(default_factory=list, init=False)
+
+    def __post_init__(self):
+        if self.name == "Manjeet":
+            raise ValueError("Error in name constraint")
+
+
+e1=employee("Manjeet2", 20, False)
+e2=employee("Manjeet1", 20, False)
+e1.country.append("US")
+print(e2)
