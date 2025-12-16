1803a3d1776a7e7c00cf1a6a7b79e6647be36442
Manjeet Singh <manjeetikon@gmail.com>
2025-12-16 21:09:47 +0530

    Added various ways to run pytest

--- Test/test_sample1.py
+++ Test/test_sample1.py
@@ -1,6 +1,14 @@
+import pytest
+
 def test_test1():
-    assert (2+3) == 5
+    assert (2 + 3) == 5
 
 def test_test2():
-    assert [1,2,3] == [1,3,4]
-    assert 1 in [1,2,3 ],"Item not present in the list"
+    assert [1, 2, 3] == [1, 2, 3]
+    assert 1 in [1, 2, 3], "Item not present in the list"
+
+def test_exception_message():
+    with pytest.raises(ZeroDivisionError) as e1:
+        10 / 0  # type: ignore
+
+    assert (str(e1.value)) == "division by zero"
