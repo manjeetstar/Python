00dfa144c894d85cf54ff50ebe6786e404082201
Manjeet Singh <manjeetikon@gmail.com>
2025-12-17 09:31:48 +0530

    Added fixture scope

--- Test/test_sample1.py
+++ Test/test_sample1.py
@@ -1,20 +1,20 @@
 import pytest
 import logging
-
 logger=logging.getLogger("Test_test_sample1")
 
-def test_test1():
-    assert (2 + 3) == 5
-    logger.info("Both numbers are equal and hence passed")
+class Test:    
+    def test_test1(self):
+        assert (2 + 3) == 5
+        logger.info("Both numbers are equal and hence passed")
+        
+    def test_test2(self):
+        assert [1, 2, 3] == [1, 2, 3]
+        assert 1 in [1, 2, 3], "Item not present in the list"
+        logger.info("Both validations are matching")
 
-def test_test2():
-    assert [1, 2, 3] == [1, 2, 3]
-    assert 1 in [1, 2, 3], "Item not present in the list"
-    logger.info("Both validations are matching")
+    def test_exception_message(self):
+        with pytest.raises(ZeroDivisionError) as e1:
+            10 / 0  # type: ignore
 
-def test_exception_message():
-    with pytest.raises(ZeroDivisionError) as e1:
-        10 / 0  # type: ignore
-
-    assert (str(e1.value)) == "division by zero"
-    logger.info("Avoid division by zero")
+        assert (str(e1.value)) == "division by zero"
+        logger.info("Avoid division by zero")
--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True, scope="session")
 def db_connection():
     print(" Initiated connection to database ")
     yield
