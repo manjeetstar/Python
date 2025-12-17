4889c572d7a00660b7a3bc8c2b2d8e3830a305c8
Manjeet Singh <manjeetikon@gmail.com>
2025-12-17 10:10:52 +0530

    Added fixture parametrization

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
-@pytest.fixture(autouse=True, scope="session")
-def db_connection():
+@pytest.fixture(autouse=True, scope="session", params=[1,2,5], ids=["Manjeet", "Parul", "Awatar"])
+def db_connection(request):
     print(" Initiated connection to database ")
-    yield
+    yield request.param
     print(" Closing db connection ")
