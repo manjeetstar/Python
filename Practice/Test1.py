911a5a910bb690fc443f5f4d9dac65559ae7616d
Manjeet Singh <manjeetikon@gmail.com>
2026-09-10 06:03:10 +0000

    Added conditional statement and documents

--- Practice/Test1.py
+++ Practice/Test1.py
@@ -1,7 +1,16 @@
 name="Manjeet"
-age=20
+age=12
 isMarried=False
 
 print(name, age, isMarried, sep=" : ", end =" ")
 print("Manjeet Singh")
 print(f"Name of candidate is {name} of age {age} and marital status is {isMarried}")
+
+if age>20:
+    print("age is greater than 20")
+elif age<10:
+    print("age is less than 10")
+elif age>5 and age<20:
+    print("Age is greater than 5 but lesser than 20")
+else:
+    print("God know that whats the age of candidate")
