46d277c1f0cb9732bd6c789b6f89a663b644e9d1
Manjeet Singh <manjeetikon@gmail.com>
2026-09-19 10:57:36 +0000

    Covered dataclass part and docs

--- Practice/Test11.py
+++ Practice/Test11.py
@@ -7,6 +7,9 @@ class Manjeet:
     age: int = 20
     address: dict = field(default_factory=dict, compare=False)
 
+    def display(self):
+        print("This is display method defined in the dataclass", self.name, self.age, self.address)
+
 m1=Manjeet("Manjeet")
 m2=Manjeet("Manjeet")
 m1.address["addressLine1"]="Essex Squaer 23"
@@ -15,6 +18,7 @@ m1.name="Parul"
 print(m1, m1.age)
 print(m2, m2.age)
 print(m1 == m2)
+m1.display()
 
 for c1 in fields(m1):
     print(c1.name, c1.type)
