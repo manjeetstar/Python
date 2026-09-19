bfb35d6f6fb0a3c6c5e502d157eda0772ef143a1
Manjeet Singh <manjeetikon@gmail.com>
2026-09-19 04:15:43 +0000

    Added more lambda expression

--- Practice/Test10.py
+++ Practice/Test10.py
@@ -7,3 +7,13 @@ ctr=[
 
 value= min(ctr, key=lambda x:x[1])
 print(value)
+
+result=(lambda x: x*2)(10)
+print(result)
+
+def calculate(x):
+    return lambda y: y*x
+
+result=calculate(5)
+print(type(result))
+print(result(20))
