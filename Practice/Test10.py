bcfd95f6d1d15d606e9619cdf1576fd2d6bc6d06
Manjeet Singh <manjeetikon@gmail.com>
2026-09-19 04:56:25 +0000

    Added FP and documentations

--- Practice/Test10.py
+++ Practice/Test10.py
@@ -17,3 +17,7 @@ def calculate(x):
 result=calculate(5)
 print(type(result))
 print(result(20))
+
+ages=[10,3,45,32,24]
+result=reduce(lambda a, b: a+b, list(map(lambda y: y*2, filter(lambda x: x%2 == 0, ages))), 0)
+print(result)
