1f1e6505348477e126ca1263d3048d7441c3757c
Manjeet Singh <manjeetikon@gmail.com>
2026-09-20 13:09:32 +0000

    Added decorator factory and updated docs

--- Practice/Test14.py
+++ Practice/Test14.py
@@ -1,14 +1,23 @@
+import time
+from functools import wraps
 
-def logicDecorator(func):
-    def wrapper(*args, **kwargs ):
-        print("This is start of the logger utility")
-        result=func(*args, **kwargs)
-        print("Sum of two numbers is ", result)
-        print("This is end of the logger utility")
-        return result
-    return wrapper
+def retry(times):
+    def logicDecorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs ):  
+            for ctr in range(1, times+1):
+                print(f"Calling {func.__name__} utility , Attempt- {ctr}")
+                start=time.perf_counter()
+                result=func(*args, **kwargs)
+                print("Sum of two numbers is ", result)
+                end=time.perf_counter()
+                print("Sum of two numbers is ", result)
+                print(f"Finished {func.__name__} utility")
+                print(f"Total time taken by the function : {end - start:.9f} seconds")                
+        return wrapper
+    return logicDecorator
 
-@logicDecorator
+@retry(times=2)
 def calculate(a, b):
     return a+b    
 
