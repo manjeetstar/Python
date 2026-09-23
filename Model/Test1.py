cf8673cd53d8e4c1562ea3121a1c012bcd0fa321
Manjeet Singh <manjeetikon@gmail.com>
2026-09-23 06:11:06 +0000

    Mutable vs Immutable

--- Model/Test1.py
+++ Model/Test1.py
@@ -4,3 +4,9 @@ ctr=["Manjeet",22,3,4,True]
 
 for stat in tracemalloc.take_snapshot().statistics("lineno")[:5]:
     print(stat)
+
+x=10
+print("Initial value ", id(x))
+
+x=20
+print("After ", id(x))
