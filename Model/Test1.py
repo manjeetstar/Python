2600ffb3cbc02c27e5fba58fc81faf3a0f47b84a
Manjeet Singh <manjeetikon@gmail.com>
2026-09-23 05:01:48 +0000

    Added memory mgmt and its docs

--- Model/Test1.py
+++ Model/Test1.py
@@ -1,5 +1,6 @@
-name="Manjeet"
-name1=name
-del name1
+import sys, tracemalloc
+tracemalloc.start()
+ctr=["Manjeet",22,3,4,True]
 
-print(globals())
+for stat in tracemalloc.take_snapshot().statistics("lineno")[:5]:
+    print(stat)
