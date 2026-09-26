79aaaeaaa6dbb976a2177bd06b23fa9e87e4db8c
Manjeet Singh <manjeetikon@gmail.com>
2026-09-26 09:43:31 +0000

    Added exception with CM and docs

--- Model/Test3.py
+++ Model/Test3.py
@@ -1,12 +1,16 @@
+
 class CManager:
     def __enter__(self):
         print("Acquiring the resources")
         return "Manjeet Singh"
 
-
     def __exit__(self, exc_type, exc, tb):
         print("Cleaning up the resources")
+        return True
 
 with CManager() as m:
     print("Main Context Manager code")
     print(f"Returned context is {m}")
+    raise ValueError("This is exception")
+
+print("Rest of the code...")
